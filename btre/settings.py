184a9d48326fdfde8c375cd020726d5025848722
Chenny <hatrend@hotmail.com>
2020-06-01 20:35:20 +0800

    ready for deployment

--- btre/settings.py
+++ btre/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
-    'realtors.apps.RealtorsConfig'
+    'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,16 @@ STATICFILES_DIRS = [
 #Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+}
+
+#Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'oneredxin@gmail.com'
+EMAIL_HOST_PASSWORD = '23232323'
+EMAIL_USE_TLS=True
